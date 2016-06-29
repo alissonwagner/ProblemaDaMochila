a162b94cccbe04f0fd9e85c9b452ba846eddf502
Alisson Wagner Rodrigues <alissonwagner93@gmail.com>
2016-06-28 23:00:02 -0300

    Criando calculo do fitness

--- src/main/java/br/com/ia/problemadamochila/service/MochilaService.java
+++ src/main/java/br/com/ia/problemadamochila/service/MochilaService.java
@@ -2,9 +2,12 @@ package br.com.ia.problemadamochila.service;
 
 import br.com.ia.problemadamochila.ParametrosForm;
 import br.com.ia.problemadamochila.bo.MochilaBO;
+import java.math.BigDecimal;
 import java.util.List;
 
 public interface MochilaService {
 
     List<MochilaBO> geraPopulacaoInicial(ParametrosForm params);
+    
+    List<MochilaBO> calculaFitnessDaPopulacao(List<MochilaBO> populacao, BigDecimal pesoMax);
 }
--- src/main/java/br/com/ia/problemadamochila/service/imp/MochilaServiceImp.java
+++ src/main/java/br/com/ia/problemadamochila/service/imp/MochilaServiceImp.java
@@ -5,6 +5,7 @@ import br.com.ia.problemadamochila.bo.ItemBO;
 import br.com.ia.problemadamochila.bo.MochilaBO;
 import br.com.ia.problemadamochila.service.MochilaService;
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.ArrayList;
 import java.util.List;
 import org.springframework.stereotype.Service;
@@ -20,7 +21,35 @@ public class MochilaServiceImp implements MochilaService{
             populacao.add(geraMochilaAleatoria(params));
         }
         
+        populacao = calculaFitnessDaPopulacao(populacao, params.getPesoMaxMochila());
+        
         return populacao;
+    }
+    
+    @Override
+    public List<MochilaBO> calculaFitnessDaPopulacao(List<MochilaBO> populacao, BigDecimal pesoMax){
+        populacao.stream().forEach((mochila) -> {
+            mochila.setFitness(calculaFitnessMochila(mochila, pesoMax));
+        });
+        
+        return populacao;
+    }
+    
+    /**
+     * Se a mochila extrapola o limite de peso, o peso dela no cálculo do fitness será 20% maior que o das mochilas no padrão
+     * O fitness será a razão entre o valor e o peso
+    */
+    private BigDecimal calculaFitnessMochila (MochilaBO mochila, BigDecimal pesoMax){
+        BigDecimal fitness;
+        BigDecimal peso = mochila.getPeso();
+        
+        if(mochila.getPeso().compareTo(pesoMax) == 1){
+            peso = mochila.getPeso().multiply(new BigDecimal("1.2"));
+        }
+        
+        fitness = mochila.getValor().divide(peso, 2,RoundingMode.HALF_UP);
+        
+        return fitness;
     }
     
     private MochilaBO geraMochilaAleatoria(ParametrosForm params){
